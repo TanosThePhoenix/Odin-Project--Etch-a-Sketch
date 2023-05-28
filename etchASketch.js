f1f8e3cd70698cc602ec67bd59f694c9bbc22c6d
TanosThePhoenix <66379918+TanosThePhoenix@users.noreply.github.com>
2023-05-28 16:16:38 -0500

    Create grid limitation size check in js

--- etchASketch.js
+++ etchASketch.js
@@ -1,4 +1,9 @@
 function createGrid(gridSize = 16) {
+	//Limit size of grid
+	if(isNaN(gridSize)) gridSize = 16;
+	else if(gridSize < 2) gridSize = 2;
+	else if(gridSize > 100) gridSize = 100;
+
 	const grid = document.createElement(div);
 	for(let r = 0; r < gridSize; ++r){
 		const gridRow = document.createElement(div);
