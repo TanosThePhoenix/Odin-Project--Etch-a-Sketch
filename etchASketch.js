57710ca349d8853ed66a5a568ad8aacb809c26a7
TanosThePhoenix <66379918+TanosThePhoenix@users.noreply.github.com>
2023-05-28 19:37:36 -0500

    Add style information for created grid in js

--- etchASketch.js
+++ etchASketch.js
@@ -4,13 +4,21 @@ function createGrid(gridSize = 16) {
 	else if(gridSize < 2) gridSize = 2;
 	else if(gridSize > 100) gridSize = 100;
 
+	const maxGridDimension = 960;
+
 	const grid = document.createElement(div);
+	grid.style.display = "flex";
+	grid.style.flexFlow = "column nowrap";
+	grid.style.height = maxGridDimension;
+	grid.style.width = maxGridDimension;
 	for(let r = 0; r < gridSize; ++r){
 		const gridRow = document.createElement(div);
-		//Set style here
+		gridRow.style.flex = "1 1 10px";
+		gridRow.style.display = "flex";
+		gridRow.style.flexFlow = "row nowrap";
 		for(let i = 0; i < gridSize; ++i){
 			const gridItem = document.createElement(div)
-			//Set style here
+			gridItem.style.flex = "1 1 10px";
 			//Insert event listener for element hover here
 			gridRow.appendChild(gridItem);
 		}
