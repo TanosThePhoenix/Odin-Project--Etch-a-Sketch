4c3b2a1f2ac4cb7b99ee8d88e0dfd27bfe77ac99
TanosThePhoenix <66379918+TanosThePhoenix@users.noreply.github.com>
2023-05-28 19:50:48 -0500

    Fix bug with createElement not getting string

--- etchASketch.js
+++ etchASketch.js
@@ -6,18 +6,18 @@ function createGrid(gridSize = 16) {
 
 	const maxGridDimension = 960;
 
-	const grid = document.createElement(div);
+	const grid = document.createElement('div');
 	grid.style.display = "flex";
 	grid.style.flexFlow = "column nowrap";
 	grid.style.height = maxGridDimension;
 	grid.style.width = maxGridDimension;
 	for(let r = 0; r < gridSize; ++r){
-		const gridRow = document.createElement(div);
+		const gridRow = document.createElement('div');
 		gridRow.style.flex = "1 1 10px";
 		gridRow.style.display = "flex";
 		gridRow.style.flexFlow = "row nowrap";
 		for(let i = 0; i < gridSize; ++i){
-			const gridItem = document.createElement(div)
+			const gridItem = document.createElement('div')
 			gridItem.style.flex = "1 1 10px";
 			gridItem.style.backgroundColor = 'white';
 			gridItem.style.border = '1px solid whitesmoke';
